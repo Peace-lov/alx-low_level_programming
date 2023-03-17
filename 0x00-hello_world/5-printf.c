e0f21883455a7adf0f300d549b4644741297313b
Peace-lov <chidieberepeace42@gmail.com>
2023-03-17 08:26:09 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
@@ -7,6 +6,7 @@
  * Return: Always 0 (Success)
  */
 int main(void)												
-{															printf("with proper grammer, but the outcome is a piece of art,\n"); 
- 	 return (0);
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
