4e638e18e9e9172f84b3d8f394907c9fb4e1b240
Peace-love <chidieberepeace42@gmail.com>
2023-03-23 10:49:07 +0000

    8 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,17 +2,17 @@
 
 /**
  * jack_bauer - prints every minute of the day of Jack Bauer
- * starting from 00:00 to 23:59
+ * where i = hour, j = minutes
  */
 void jack_bauer(void)
 {
 	int i, j;
 
 	i = 0;
-	j = 0;
 
 	while (i < 24)
 	{
+		j = 0;
 		while (j < 60)
 		{
 			_putchar((i / 10) + '0');
