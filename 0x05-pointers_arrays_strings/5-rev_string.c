314922141444b1fd01a816f937a83364b524e3cf
Peace-love <chidieberepeace42@gmail.com>
2023-03-28 14:37:48 +0000

    modified:   5-rev_string.c new file:   6-puts2.c  new file:   7-puts_half.c  new file:   8-print_array.c new file:   9-strcpy.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,9 +7,17 @@
  */
 void rev_string(char *s)
 {
-	int b = 0;
+	char rev = s[0];
+	int counter = 0;
+	int i;
 
-	while (s[b] != '\0')
-		--b;
+	while (s[counter] != '\0')
+	counter++;
+	for (i = 0; i < counter; i++)
 	{
-		_putchar
+		counter--;
+		rev = s[i];
+		s[i] = s[counter];
+		s[counter] = rev;
+	}
+}
