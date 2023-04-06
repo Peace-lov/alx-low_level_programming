1eead1b7eb924aaacfd433732dd63e86fa6d8785
Peace-love <chidieberepeace42@gmail.com>
2023-04-06 12:05:38 +0000

    My commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,11 +10,11 @@
 
 void _puts_recursion(char *s)
 {
-	int z = 0;
-
-	for (; s[z] != '\0'; z++)
+	if (*s == '\0')
 	{
-		_putchar(s[z]);
+		_putchar('\n');
+		return;
 	}
-	_putchar('\n');
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
