5817eefdf5de42543c0412674e231916da2829a5
Peace-love <chidieberepeace42@gmail.com>
2023-04-07 22:40:31 +0000

    2-Args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all arguments it receives
+ * @argc: argument count
+ * @argv: array of size
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int a = 0;
+
+	for (a = 0; a < argc; a++)
+	{
+		printf("%s \n", argv[a]);
+	}
+	return (0);
+}
