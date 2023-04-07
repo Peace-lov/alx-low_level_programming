d792bc73fb2083904ca445851a50707b814e7b11
Peace-love <chidieberepeace42@gmail.com>
2023-04-07 22:08:03 +0000

    Args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print the number of arguements passed to it
+ * @argc: arguement count
+ * @argv: array of size
+ *
+ * Return: 0;
+ */
+
+int main(int argc, __attribute__ ((unused)) char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return 0;
+}
