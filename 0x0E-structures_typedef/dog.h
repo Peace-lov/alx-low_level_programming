42cefb945e3e153b2e65b567e97d2bde6b4d1f25
Peace-love <chidieberepeace42@gmail.com>
2023-04-18 21:29:51 +0000

    Dogh

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Define a new type dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: 0
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
