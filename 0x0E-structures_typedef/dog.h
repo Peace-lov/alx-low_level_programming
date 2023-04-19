484c56a322721cba6763db0fbf6717c1aec9e4db
Peace-love <chidieberepeace42@gmail.com>
2023-04-19 00:01:36 +0000

    My commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- * struct dog - Define a new type dog
+ * struct dog_t - Define a new type dog
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
@@ -17,5 +17,6 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef struct dog dog_t;
 
 #endif
