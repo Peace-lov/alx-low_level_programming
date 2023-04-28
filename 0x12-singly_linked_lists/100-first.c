960f2f81c1d765593fdcd4364fb42f8a250fc58f
Peace-lov <chidieberepeace42@gmail.com>
2023-04-28 18:12:18 +0000

    My commit

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,5 +9,6 @@ void Best(void) __attribute__((constructor));
  */
 void Best(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
