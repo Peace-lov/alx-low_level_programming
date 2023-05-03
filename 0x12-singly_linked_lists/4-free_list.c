651e18e6e8229131240fdd59326d05626e73109b
Peace-lov <chidieberepeace42@gmail.com>
2023-05-03 00:08:15 +0000

    commit

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -11,8 +11,9 @@ void free_list(list_t *head)
 
 	while (head)
 	{
-		temp = head->next;
-		free(head);
-		head = temp;
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
 	}
 }
