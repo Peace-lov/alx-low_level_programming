94b851ea94d39093f135bc8dde04c543a85cb604
Peace-lov <chidieberepeace42@gmail.com>
2023-06-21 21:01:04 +0000

    function_pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - function that prints a name
+ * @name: Name of array
+ * @f: function pointer
+ *
+ * Return: 0
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
