6c8b3a20b853c1230f387f3e4e6de4a6d0b0e635
Peace-love <chidieberepeace42@gmail.com>
2023-06-23 13:16:00 +0000

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -26,7 +26,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("nil");
 		}
 		printf("%s", str);
-		if (separator && a < (n - 1))
+		if (separator && a != (n - 1))
 		{
 			printf("%s", separator);
 		}
