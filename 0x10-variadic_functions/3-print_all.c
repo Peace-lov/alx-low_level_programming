590337cbd9ec688fef3b43265f2df90b3790b0c5
Peace-love <chidieberepeace42@gmail.com>
2023-06-24 17:10:04 +0000

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,38 +9,42 @@ void print_all(const char * const format, ...)
 {
 	va_list anything;
 	char *str;
-	int k;
+	unsigned int k = 0, c, i;
+	float f;
 	char *opera = "";
 
 	va_start(anything, format);
-	if (format)
+	while (format[k])
 	{
 		if (format[k])
 		{
-			switch(format[k])
+			switch (format[k])
 			{
-				case "c":
-					printf("%s%d\n", opera, va_arg(anything, int));
+				case 'c':
+					c = (char)va_arg(anything, int);
+					printf("%s%c", opera, c);
 					break;
-				case "i":
-					printf("%s%i", opera, va_arg(anything, int));
+				case 'i':
+					i = va_arg(anything, int);
+					printf("%s%i", opera, i);
 					break;
-				case "f":
-					printf("%s%f", opera, va_arg(anything, double));
+				case 'f':
+					f = (float)va_arg(anything, double);
+					printf("%s%f", opera, f);
 					break;
-				case "s":
+				case 's':
 					str = va_arg(anything, char *);
 					if (str == NULL)
 					{
-						printf("(nil)");
+						printf("nil");
 					}
-					printf("%s%s", opera, va_arg(anything, char*));
+					printf("%s%s", opera, str);
 					break;
 				default:
 					k++;
 					continue;
 			}
-			opera = ",";
+			opera = ", ";
 			k++;
 		}
 	}
