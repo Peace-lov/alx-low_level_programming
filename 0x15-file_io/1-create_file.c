84fdf411a391651d119e2b33590226dfd9f3e137
Peace-love <chidieberepeace42@gmail.com>
2023-07-14 17:31:47 +0000

    file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,14 +16,13 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
-	if (fd < 0)
+	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)
 	{
 		while (text_content[len])
 			len++;
 		bywrt = write(fd, text_content, len);
-		
 		if (bywrt != len)
 			return (-1);
 	}
