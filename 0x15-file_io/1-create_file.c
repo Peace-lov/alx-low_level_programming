63e2a1511d52f4d7a56afbf99db593b8b11b2315
Peace-love <chidieberepeace42@gmail.com>
2023-07-14 17:15:13 +0000

    file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,32 +1,33 @@
 #include "main.h"
 /**
- * create_file - Create a function that creates a file.
- * @filename: Name of the file
+ * create_file - Create a function that creates a file
+ * @filename: Name of file
  * @text_content: content of the file
  *
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file_descriptor, i = 0, len = 0;
+	int fd, len = 0;
 	int bywrt;
 
 	if (filename == NULL)
 		return (-1);
 
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
 	if (text_content != NULL)
 	{
-		while (text_content[i])
-			len = i++;
+		while (text_content[len])
+			len++;
+		bywrt = write(fd, text_content, len);
+		
+		if (bywrt != len)
+			return (-1);
 	}
-	file_descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
-	bywrt = write(file_descriptor, text_content, len);
-
-	if (!bywrt)
-		return (-1);
-
-	close(file_descriptor);
-
+	close(fd);
 	return (1);
 }
