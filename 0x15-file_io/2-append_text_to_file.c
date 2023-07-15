33d8ade152fac0b1362de9fc04ad530df03ab7fc
Peace-love <chidieberepeace42@gmail.com>
2023-07-15 01:08:36 +0000

    file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	write_file = write(file_des, text_content, len);
 
-	if (!file_des || !write_file)
+	if (file_des == -1 || write_file == -1)
 		return (-1);
 	close(file_des);
 	return (1);
