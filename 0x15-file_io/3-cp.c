d22c17a66ec21e2cba3f4f6bf4acd2c416358bdc
Peace-lov <chidieberepeace42@gmail.com>
2023-08-26 20:44:07 +0100

    copy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,9 +4,9 @@
  * @f_d: close fd
  * Return: Returns nothing
  */
-void exit_err(int f_d)
+void exit_err(int fd)
 {
-	dprintf(STDERR_FILENO, "Error: Can't close f_d %d\n", f_d);
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 	exit(100);
 }
 /**
@@ -20,8 +20,7 @@ int main(int ac, char *av[])
 	int fd_from, fd_to;
 	char buf[BUF_SZ];
 	ssize_t bytrd;
-	const char *file_from = av[1];
-	const char *file_to = av[2];
+	const char *file_from = av[1], *file_to = av[2];
 
 	if (ac != 3)
 	{
@@ -31,26 +30,26 @@ int main(int ac, char *av[])
 	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", file_from);
 		exit(98);
 	}
 	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, F_P);
 	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_to);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 		exit(99);
 	}
 	while ((bytrd = read(fd_from, buf, BUF_SZ)) > 0)
 	{
 		if (write(fd_to, buf, bytrd) == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_to);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 			exit(99);
 		}
 	}
 	if (bytrd == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", file_from);
 		exit(98);
 	}
 	if (close(fd_from) == -1 || close(fd_to) == -1)
