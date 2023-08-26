6e096df63b58adf8869fdac3dcabac3c025103ca
Peace-lov <chidieberepeace42@gmail.com>
2023-08-26 14:46:40 +0100

    copy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -27,28 +27,33 @@ int main(int ac, char *av[])
 
 	if (ac != 3)
 	{
-		exit_err(97, "Usage: cp file_from file_to");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
 	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
 	{
-		exit_err(98, "Can't read from file %s", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
 	}
 	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, F_P);
 	if (fd_to == -1)
 	{
-		exit_err(99, "Can't write to file %s", file_to);
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_to);
+		exit(99);
 	}
 	while ((bytrd = read(fd_from, buf, BUF_SZ)) > 0)
 	{
 		if (write(fd_to, buf, bytrd) == -1)
 		{
-			exit_err(99, "Can't write to file %s", file_to);
+			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_to);
+			exit(99);
 		}
 	}
 	if (bytrd == -1)
 	{
-		exit_err(98, "Can't read from file %s", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
 	}
 	if (close(fd_from) == -1 || close(fd_to) == -1)
 	{
