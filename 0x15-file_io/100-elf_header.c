5189c8dce13c5b30a0f352bb649c75cff6219327
Peace-lov <chidieberepeace42@gmail.com>
2023-09-08 16:44:48 -0700

    100

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,5 +1,14 @@
 #include "main.h"
 /**
+ * p_err - Prints error to stdout
+ * @err_message: The error message to print
+ * Return: Nothing
+ */
+void p_err(const char *err_message)
+{
+	fprintf(stderr, "%s\n", err_message);
+}
+/**
  * elf_header - prints elf header information
  * @name: pointer
  *
@@ -12,49 +21,41 @@ void elf_header(const char *name)
 
 	if (f_d == -1)
 	{
-		printf("Error opening file.\n");
-		exit(98);
+		p_err("Error opening file.");
 	}
 	if (read(f_d, &h, sizeof(Elf64_Ehdr)) != sizeof(Elf64_Ehdr))
 	{
-		printf("Error reading ELF header.\n");
-		close(f_d);
-		exit(98);
+		p_err("Error reading ELF header.");
 	}
-	if (h.e_ident[0] != 0x7F ||
-			h.e_ident[1] != 'E' ||
-			h.e_ident[2] != 'L' ||
-			h.e_ident[3] != 'F')
+	if (h.e_ident[0] != 0x7F || h.e_ident[1] != 'E'
+	|| h.e_ident[2] != 'L' || h.e_ident[3] != 'F')
 	{
-		printf("Not an ELF file.\n");
-		close(f_d);
-		exit(98);
+		p_err("Not an ELF file.");
 	}
-	printf("Magic: %02x %02x %02x %02x\n", h.e_ident[0], h.e_ident[1], h.e_ident[2], h.e_ident[3]);
+
+	printf("Magic: %02x %02x %02x %02x\n", h.e_ident[0],
+	h.e_ident[1], h.e_ident[2], h.e_ident[3]);
 	printf("Class: %d-bit\n", h.e_ident[4] == 1 ? 32 : 64);
-	printf("Data: %s\n", h.e_ident[5] == 1 ? "Little-endian" : "Big-endian");
+	printf("Data: %s\n", h.e_ident[5] == 1 ? "Little-endian" :
+	"Big-endian");
 	printf("Version: %d\n", h.e_ident[6]);
 	printf("OS/ABI: %d\n", h.e_ident[7]);
 	printf("ABI Version: %d\n", h.e_ident[8]);
 	printf("Type: %d\n", h.e_type);
-	printf("Entry point address: 0x%lx\n", (unsigned long) h.e_entry);
+	printf("Entry point address: 0x%lx\n", (unsigned long)h.e_entry);
+
 	close(f_d);
 }
-/**
- * main - check for Elf file
- * @ac: argument count
- * @av: argument vector
- * Return: 0
- */
+
 int main(int ac, char *av[])
 {
 	if (ac != 2)
 	{
-		printf("Usage: %s elf_filename\n", av[0]);
+		fprintf(stderr, "Usage: %s elf_filename\n", av[0]);
 		return (1);
 	}
 
 	elf_header(av[1]);
 
-	return (0);
+	return 0;
 }
